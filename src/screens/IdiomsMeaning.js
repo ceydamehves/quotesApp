b3fce090155914322e38cd1f164abd0c93eab6b1
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2021-02-08 18:16:38 +0300

    TR - EN screens main design.

--- src/screens/IdiomsMeaning.js
+++ src/screens/IdiomsMeaning.js
@@ -7,11 +7,90 @@ import {useNavigation} from '@react-navigation/native';
 const IdiomsMeaning = () => {
   const navigation = useNavigation();
   return (
-    <View style={{ flex: 1 }}>
-      <Text> Idiom Meaning Screen</Text>
-    </View>
+    <View style={{ flex: 1, backgroundColor:'#fff' }}>
+      <View style={styles.header}>        
+        <TouchableOpacity style={styles.backButton}
+                          onPress={() => {navigation.navigate('Home')}}>
+        <Image style={styles.backImage}
+                        source={require ('../assets/back.png')}/>
+        </TouchableOpacity>
+        <Image style={styles.headerTitle}
+                        source={require ('../assets/Idioms.png')}/>
+      </View>
+
+      <ScrollView style={{ flex: 1 , alignSelf:'center'}}>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>TR</Text>
+        </View>
+        <View style={styles.textHolder}>
+          <Text style={{alignSelf:'center'}}>bla bla bla</Text>
+        </View>
+
+        <View style={styles.title}>
+          <Text style={styles.titleText}>EN</Text>
+        </View>
+        <View style={styles.textHolder}>
+          <Text style={{alignSelf:'center'}}>bla bla bla</Text>
+        </View>
+
+      </ScrollView>
+      </View>
   );
 }
 
 export default IdiomsMeaning;
+
+const styles = StyleSheet.create({
+  header: {
+    alignSelf:'center',
+    width: Dimensions.get('window').width * 1,
+    height: Dimensions.get('window').height * 0.25,
+    backgroundColor:'#E76F93', 
+    borderBottomRightRadius: 180,
+    borderBottomLeftRadius: 180,
+    elevation: 8
+  },
+  headerTitle: {
+    alignSelf:'center',
+    width: Dimensions.get('window').width * 0.4,
+    height: Dimensions.get('window').height * 0.1,
+  },
+  backButton: {
+    alignSelf:'center',
+    marginTop: 75,
+    marginRight: 130,
+    height: 25,
+    width: 25,
+  },
+  backImage: {
+    alignSelf:'center',
+    height: 27,
+    width: 25,
+  },
+  title: {
+    marginTop:20,
+    marginRight: 300, 
+    height:30, 
+    width:50 , 
+    backgroundColor:'#E76F93', 
+    borderRadius: 20, 
+    elevation:5
+  },
+  titleText: {
+    alignSelf:'center', 
+    marginTop: 4, 
+    color:'#fff', 
+    fontSize:16, 
+    fontWeight:'bold'
+  },
+  textHolder: {
+    backgroundColor:'#fff',
+    marginTop:10, 
+    width: Dimensions.get('window').width * 0.9,
+    height: Dimensions.get('window').height * 0.12, 
+    borderRadius: 30, 
+    borderWidth: 3, 
+    borderColor:'#E76F93'
+  }
+})
 
--- src/screens/QuotesMeaning.js
+++ src/screens/QuotesMeaning.js
@@ -4,14 +4,93 @@ import { StyleSheet,Dimensions } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
 
-const QuotesMeaning = () => {
+const IdiomsMeaning = () => {
   const navigation = useNavigation();
   return (
-    <View style={{ flex: 1 }}>
-      <Text> Quote Meaning Screen</Text>
-    </View>
+    <View style={{ flex: 1, backgroundColor:'#fff' }}>
+      <View style={styles.header}>        
+        <TouchableOpacity style={styles.backButton}
+                          onPress={() => {navigation.navigate('Home')}}>
+        <Image style={styles.backImage}
+                        source={require ('../assets/back.png')}/>
+        </TouchableOpacity>
+        <Image style={styles.headerTitle}
+                        source={require ('../assets/Quotes.png')}/>
+      </View>
+
+      <ScrollView style={{ flex: 1 , alignSelf:'center'}}>
+        <View style={styles.title}>
+          <Text style={styles.titleText}>TR</Text>
+        </View>
+        <View style={styles.textHolder}>
+          <Text style={{alignSelf:'center'}}>bla bla bla</Text>
+        </View>
+
+        <View style={styles.title}>
+          <Text style={styles.titleText}>EN</Text>
+        </View>
+        <View style={styles.textHolder}>
+          <Text style={{alignSelf:'center'}}>bla bla bla</Text>
+        </View>
+
+      </ScrollView>
+      </View>
   );
 }
 
-export default QuotesMeaning;
+export default IdiomsMeaning;
+
+const styles = StyleSheet.create({
+  header: {
+    alignSelf:'center',
+    width: Dimensions.get('window').width * 1,
+    height: Dimensions.get('window').height * 0.25,
+    backgroundColor:'#7C76C9', 
+    borderBottomRightRadius: 180,
+    borderBottomLeftRadius: 180,
+    elevation: 8
+  },
+  headerTitle: {
+    alignSelf:'center',
+    width: Dimensions.get('window').width * 0.4,
+    height: Dimensions.get('window').height * 0.1,
+  },
+  backButton: {
+    alignSelf:'center',
+    marginTop: 75,
+    marginRight: 130,
+    height: 25,
+    width: 25,
+  },
+  backImage: {
+    alignSelf:'center',
+    height: 27,
+    width: 25,
+  },
+  title: {
+    marginTop:20,
+    marginRight: 300, 
+    height:30, 
+    width:50 , 
+    backgroundColor:'#7C76C9', 
+    borderRadius: 20, 
+    elevation:5
+  },
+  titleText: {
+    alignSelf:'center', 
+    marginTop: 4, 
+    color:'#fff', 
+    fontSize:16, 
+    fontWeight:'bold'
+  },
+  textHolder: {
+    backgroundColor:'#fff',
+    marginTop:10, 
+    width: Dimensions.get('window').width * 0.9,
+    height: Dimensions.get('window').height * 0.12, 
+    borderRadius: 30, 
+    borderWidth: 3, 
+    borderColor:'#7C76C9'
+  }
+})
 
