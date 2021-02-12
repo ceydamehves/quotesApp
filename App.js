c889895ec437af19ea898334ffbc3f9c735c91f8
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2021-02-12 19:20:54 +0300

    firebase and push notification added.

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
+import PushNotification from "react-native-push-notification";
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/screens/navigation/AppNavigator.js';
+import Firebase from '@react-native-firebase/app';
 
-const App = () => {
- return (
-  <NavigationContainer>
-   <AppNavigator />
-  </NavigationContainer>
+class App extends React.Component{
+    componentDidMount(){
+        Firebase.initializeApp(this)
+        PushNotification.configure({
+            // (optional) Called when Token is generated (iOS and Android)
+            onRegister: function (token) {
+              console.log("TOKEN:", token);
+            },
+          
+            // (required) Called when a remote is received or opened, or local notification is opened
+            onNotification: function (notification) {
+              console.log("NOTIFICATION:", notification);
+          
+              // process the notification
+          
+              // (required) Called when a remote is received or opened, or local notification is opened
+              notification.finish(PushNotification.FetchResult.NoData);
+            },
+          
+            // Should the initial notification be popped automatically
+            // default: true
+            popInitialNotification: true,
+          
+            /**
+             * (optional) default: true
+             * - Specified if permissions (ios) and token (android and ios) will requested or not,
+             * - if not, you must call PushNotificationsHandler.requestPermissions() later
+             * - if you are not using remote notification or do not have Firebase installed, use this:
+             *     requestPermissions: Platform.OS === 'ios'
+             */
+            requestPermissions: true,
+          });
+    }
+
+  
+  render(){
+    return (
+
+        <NavigationContainer>
+        <AppNavigator />
+        </NavigationContainer>
  );
 }
-
+}
 export default App;
